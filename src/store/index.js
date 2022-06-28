ee9716e2f9cbe0d0551e2a3a7d44f98042839835
Ivanoiu Petrut <ivanoiu.petrut27@yahoo.com>
2022-06-28 15:46:28 +0300

    feat(api): add forecast weather fetch Introduced a request to the API then storing the data for a set number of days in a vector

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,34 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
 
 export default createStore({
   state: {
+    baseUrl: "http://api.weatherapi.com/v1/",
+    weatherType: "forecast",
+    city: "Sibiu",
+    country: "Romania",
+    nextDays: [],
   },
-  getters: {
-  },
+  getters: {},
   mutations: {
+    setNextDays(state, nextDays) {
+      state.nextDays = nextDays;
+      console.log("hello");
+    },
   },
   actions: {
+    fetchForecast({ commit }) {
+      fetch(
+        "https://api.weatherapi.com/v1/forecast.json?key=c48712edce2441edae5122038222706&q=Sibiu&days=10&aqi=no&alerts=no"
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data.forecast.forecastday);
+          commit("setNextDays", data.forecast.forecastday);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
   },
-  modules: {
-  }
-})
+  modules: {},
+});
