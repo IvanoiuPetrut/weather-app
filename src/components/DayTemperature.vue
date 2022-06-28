a5753e54e21ef4476acfb182b5e38b637b33d0be
Ivanoiu Petrut <ivanoiu.petrut27@yahoo.com>
2022-06-28 15:52:24 +0300

    feat: add HTML template

---
+++ src/components/DayTemperature.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="card">
+    <h3 class="day"></h3>
+    <img src="http://placekitten.com/100/100" alt="cat" />
+    <div class="temperature">
+      <p class="temperature__high">H:</p>
+      <p class="temperature__low">L:</p>
+    </div>
+    <p class="weather">mostly sunny</p>
+  </div>
+</template>
