2a1e5e3a49303206b211dcd88b11ba3da26a4ec2
Ivanoiu Petrut <ivanoiu.petrut27@yahoo.com>
2022-06-28 16:52:57 +0300

    feat: add DayTemperature component to App template

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
-<template></template>
+<template>
+  <DayTemperature></DayTemperature>
+  <button @click="$store.dispatch('fetchForecast')">Refresh</button>
+</template>
 
 <script>
+import DayTemperature from "./components/DayTemperature.vue";
+
 export default {
   name: "App",
-  components: {},
+  components: {
+    DayTemperature,
+  },
 };
 </script>
 
