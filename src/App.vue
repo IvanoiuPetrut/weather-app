b63375e43e10081306d7a49ee7debf0ab416acb2
Ivanoiu Petrut <ivanoiu.petrut27@yahoo.com>
2022-06-29 15:47:51 +0300

    test: add a bg img to test the opacity of the card component

--- src/App.vue
+++ src/App.vue
@@ -39,6 +39,14 @@ html {
   line-height: 1.5;
 }
 
+body {
+  background-image: url("https://images.unsplash.com/photo-1448375240586-882707db888b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80");
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+}
+
 .container {
   max-width: 1200px;
   margin: 0 auto;
