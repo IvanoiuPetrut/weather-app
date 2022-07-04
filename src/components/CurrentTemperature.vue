a48968c36a9956dd407a7727e8ffc6343f20e3f5
Petrut <ivanoiu.petrut27@yahoo.com>
2022-07-04 21:48:55 +0300

    fix(style): add scoped css

--- src/components/CurrentTemperature.vue
+++ src/components/CurrentTemperature.vue
@@ -1,13 +1,33 @@
 <template>
-  <h2 class="location">Sibiu, Romania</h2>
-  <div class="temperature__wrapper">
-    <img src="ss" alt="temperature" class="temperature__image" />
-    <div class="temperature">
-      <p class="temperature__now">23 C</p>
-      <div>
-        <p class="temperature__high">H: 30 C</p>
-        <p class="temperature__low">L: 18 C</p>
+  <div class="card">
+    <h2 class="location">Sibiu, Romania</h2>
+    <div class="temperature__wrapper">
+      <!-- <img src="ss" alt="temperature" class="temperature__image" /> -->
+      <div class="temperature">
+        <p class="temperature__now">23 C</p>
+        <div>
+          <p class="temperature__high">H: 30 C</p>
+          <p class="temperature__low">L: 18 C</p>
+        </div>
       </div>
     </div>
   </div>
 </template>
+
+<script>
+import { mapState } from "vuex";
+export default {
+  name: "CurrentTemperature",
+  computed: {
+    ...mapState({
+      isCelsius: (state) => state.isCelsius,
+    }),
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+* {
+  color: #fff;
+}
+</style>
