36db69a93eccdf9e6d48ead7062d91f12af53b32
Petrut <ivanoiu.petrut27@yahoo.com>
2022-07-04 21:33:51 +0300

    feat: add CurrentTemperature HTML template

---
+++ src/components/CurrentTemperature.vue
@@ -0,0 +1,13 @@
+<template>
+  <h2 class="location">Sibiu, Romania</h2>
+  <div class="temperature__wrapper">
+    <img src="ss" alt="temperature" class="temperature__image" />
+    <div class="temperature">
+      <p class="temperature__now">23 C</p>
+      <div>
+        <p class="temperature__high">H: 30 C</p>
+        <p class="temperature__low">L: 18 C</p>
+      </div>
+    </div>
+  </div>
+</template>
