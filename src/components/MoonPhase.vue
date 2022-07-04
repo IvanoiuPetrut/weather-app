ba73fa99d452ee71c5f4296a4aff4f851ddb47bd
Petrut <ivanoiu.petrut27@yahoo.com>
2022-07-04 19:20:12 +0300

    feat: add computed propierties from store in HTML template

--- src/components/MoonPhase.vue
+++ src/components/MoonPhase.vue
@@ -1,17 +1,58 @@
 <template>
-  <p class="date">june 20, 2022</p>
-  <p class="moon-phase">Full Moon</p>
+  <p class="date">{{ currentDate }}</p>
+  <p class="moon-phase">{{ moonPhase }}</p>
   <div class="astro">
     <div class="astro__sun">
-      <p class="astro__sunrise">12 am</p>
-      <p class="astro__sunset">10 pm</p>
+      <p class="astro__sunrise">{{ sunRise }}</p>
+      <p class="astro__sunset">{{ sunSet }}</p>
     </div>
     <div class="astro__moon">
-      <p class="astro__moonrise">10 pm</p>
-      <p class="astro__moonset">5 am</p>
+      <p class="astro__moonrise">{{ moonRise }}</p>
+      <p class="astro__moonset">{{ moonSet }}</p>
     </div>
   </div>
 </template>
+
+<script>
+import { mapState } from "vuex";
+export default {
+  name: "MoonPhase",
+  methods: {
+    formatDate(dateStr) {
+      var date = new Date(dateStr);
+      return date.toLocaleDateString("en-US", {
+        month: "long",
+        day: "numeric",
+        year: "numeric",
+      });
+    },
+  },
+  computed: {
+    ...mapState({
+      date: (state) => state.date,
+      astronomy: (state) => state.astronomy,
+    }),
+    currentDate() {
+      return this.formatDate(this.date);
+    },
+    moonPhase() {
+      return this.astronomy.moon_phase;
+    },
+    sunRise() {
+      return this.astronomy.sunrise;
+    },
+    sunSet() {
+      return this.astronomy.sunset;
+    },
+    moonRise() {
+      return this.astronomy.moonrise;
+    },
+    moonSet() {
+      return this.astronomy.moonset;
+    },
+  },
+};
+</script>
 
 <style lang="scss" scoped>
 * {
