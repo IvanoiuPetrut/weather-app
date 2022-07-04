a8514fbefd1b712e4eac629f481cffe5ffb44154
Petrut <ivanoiu.petrut27@yahoo.com>
2022-07-04 18:21:05 +0300

    feat: add HTML template for MoonPhase

---
+++ src/components/MoonPhase.vue
@@ -0,0 +1,14 @@
+<template>
+  <p class="date">june 20, 2022</p>
+  <p class="moon-phase">Full Moon</p>
+  <div class="astro">
+    <div class="astro__sun">
+      <p class="astro__sunrise">12 am</p>
+      <p class="astro__sunset">10 pm</p>
+    </div>
+    <div class="astro__moon">
+      <p class="astro__moonrise">10 pm</p>
+      <p class="astro__moonset">5 am</p>
+    </div>
+  </div>
+</template>
