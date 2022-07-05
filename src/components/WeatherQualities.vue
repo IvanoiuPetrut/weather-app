243abc32ba2456fc7d742959553825508f64cc25
Ivanoiu Petrut <ivanoiu.petrut27@yahoo.com>
2022-07-05 14:58:33 +0300

    feat: add HTML template for Weather Qualities component

---
+++ src/components/WeatherQualities.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="card">
+    <div>
+      <p>Air Quality</p>
+      <p>22</p>
+    </div>
+    <div>
+      <p>Precipitation</p>
+      <p>22</p>
+    </div>
+    <div>
+      <p>Wind</p>
+      <p>22</p>
+    </div>
+    <div>
+      <p>Pressure</p>
+      <p>22</p>
+    </div>
+    <div>
+      <p>Visibility</p>
+      <p>22</p>
+    </div>
+    <div>
+      <p>UV Index</p>
+      <p>2</p>
+    </div>
+  </div>
+</template>
