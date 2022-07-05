54b5f1be39b4aa8ce4630f82d7a21cdf510c6d24
Ivanoiu Petrut <ivanoiu.petrut27@yahoo.com>
2022-07-05 16:34:23 +0300

    feat: create CurrentWeather component which encapsulates WeatherQualities, CurrentTemperature, MoonPhase

---
+++ src/components/CurrentWeather.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="weather">
+    <WeatherQualities></WeatherQualities>
+    <CurrentTemperature></CurrentTemperature>
+    <MoonPhase></MoonPhase>
+  </div>
+</template>
+
+<script>
+import WeatherQualities from "./WeatherQualities.vue";
+import CurrentTemperature from "./CurrentTemperature.vue";
+import MoonPhase from "./MoonPhase.vue";
+
+export default {
+  name: "CurrentWeather",
+  components: {
+    WeatherQualities,
+    CurrentTemperature,
+    MoonPhase,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.weather {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 4.8rem;
+}
+</style>
