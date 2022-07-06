59a8a0b5606bffeaf097d59e404788ebe125cb65
Ivanoiu Petrut <ivanoiu.petrut27@yahoo.com>
2022-07-06 17:59:35 +0300

    feat: add HTML template for SearchBar

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,19 @@
+<template>
+  <input type="text" v-model="searchText" placeholder="Enter city" />
+</template>
+
+<script>
+export default {
+  name: "SearchBar",
+  data() {
+    return {
+      searchText: "",
+    };
+  },
+  methods: {
+    search() {
+      this.$store.dispatch("search", this.searchText);
+    },
+  },
+};
+</script>
