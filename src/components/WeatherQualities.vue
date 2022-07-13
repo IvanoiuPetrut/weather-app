e47274ea15bbc52f936b55a3d98a7e7bb27be2d4
Petrut <ivanoiu.petrut27@yahoo.com>
2022-07-13 15:07:53 +0300

    feat: add current weather qualitie for the next 8 hours

--- src/components/WeatherQualities.vue
+++ src/components/WeatherQualities.vue
@@ -34,6 +34,7 @@ export default {
   name: "WeatherQualities",
   data() {
     return {
+      currentDayId: 0,
       uvIndexTable: [
         {
           index: 2,
@@ -57,6 +58,8 @@ export default {
   computed: {
     ...mapState({
       currentWeather: (state) => state.currentWeather,
+      forecast: (state) => state.forecast,
+      hourIndex: (state) => state.hourIndex,
       isKmH: (state) => state.settings.isKmH,
       isMm: (state) => state.settings.isMm,
       isMb: (state) => state.settings.isMb,
@@ -67,26 +70,40 @@ export default {
     },
     precipitation() {
       return this.isMm
-        ? `${this.currentWeather.current.precip_mm} mm`
-        : `${this.currentWeather.current.precip_in} in`;
+        ? `${
+            this.forecast[this.currentDayId].hour[this.hourIndex].precip_mm
+          } mm`
+        : `${
+            this.forecast[this.currentDayId].hour[this.hourIndex].precip_in
+          } in`;
     },
     wind() {
       return this.isKmH
-        ? `${this.currentWeather.current.wind_kph} km/h`
-        : `${this.currentWeather.current.wind_mph} mph`;
+        ? `${
+            this.forecast[this.currentDayId].hour[this.hourIndex].wind_kph
+          } km/h`
+        : `${
+            this.forecast[this.currentDayId].hour[this.hourIndex].wind_mph
+          } mph`;
     },
     pressure() {
       return this.isMb
-        ? `${this.currentWeather.current.pressure_mb} mb`
-        : `${this.currentWeather.current.pressure_in} in`;
+        ? `${
+            this.forecast[this.currentDayId].hour[this.hourIndex].pressure_mb
+          } mb`
+        : `${
+            this.forecast[this.currentDayId].hour[this.hourIndex].pressure_in
+          } in`;
     },
     visibility() {
       return this.isKm
-        ? `${this.currentWeather.current.vis_km} km`
-        : `${this.currentWeather.current.vis_miles} miles`;
+        ? `${this.forecast[this.currentDayId].hour[this.hourIndex].vis_km} km`
+        : `${
+            this.forecast[this.currentDayId].hour[this.hourIndex].vis_miles
+          } miles`;
     },
     uvIndex() {
-      return this.currentWeather.current.uv;
+      return this.forecast[this.currentDayId].hour[this.hourIndex].uv;
     },
     howGoodIsUvIndex() {
       return this.uvIndexTable.find(
