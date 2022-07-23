59f5a045839447b9f41b4bf81891a2635b447409
Petrut <ivanoiu.petrut27@yahoo.com>
2022-07-23 18:26:08 +0300

    fix: removed forecast from App

--- src/App.vue
+++ src/App.vue
@@ -4,17 +4,6 @@
   </nav>
   <div v-if="this.city.length > 0">
     <CurrentWeather class="current-weather"></CurrentWeather>
-    <div class="forecast container">
-      <h2 class="heading--tertiary">3-day forecast</h2>
-      <div class="days-temperature">
-        <DayTemperature
-          v-for="(day, index) in $store.state.forecast"
-          :key="index"
-          :currentDayId="index"
-          :dayIndex="this.dayIndex"
-        ></DayTemperature>
-      </div>
-    </div>
   </div>
   <div v-else>
     <h2 class="heading--tertiary">Please enter a city</h2>
@@ -24,14 +13,12 @@
 <script>
 import { mapState } from "vuex";
 import { geoLocation } from "./helpers/geoLocation";
-import DayTemperature from "./components/DayTemperature.vue";
 import CurrentWeather from "./components/CurrentWeather.vue";
 import Meniu from "./components/Meniu.vue";
 
 export default {
   name: "App",
   components: {
-    DayTemperature,
     CurrentWeather,
     Meniu,
   },
@@ -43,7 +30,6 @@ export default {
   },
   computed: {
     ...mapState({
-      dayIndex: (state) => state.dayIndex,
       city: (state) => state.city,
     }),
   },
@@ -76,40 +62,4 @@ export default {
 };
 </script>
 
-<style lang="scss">
-@mixin glass-morph {
-  background: rgba(255, 255, 255, 0.15);
-  box-shadow: 0 8px 32px 0 rgba(34, 34, 35, 0.37);
-  backdrop-filter: blur(4px);
-  -webkit-backdrop-filter: blur(4px);
-  border-radius: 10px;
-  border: 1px solid rgba(255, 255, 255, 0.18);
-}
-
-body {
-  background-image: linear-gradient(
-      to bottom,
-      rgba(0, 0, 0, 0.7),
-      rgba(0, 0, 0, 0.3)
-    ),
-    url("https://images.unsplash.com/photo-1448375240586-882707db888b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80");
-  background-size: cover;
-  background-position: center;
-  background-repeat: no-repeat;
-  background-attachment: fixed;
-}
-
-.forecast {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: 0.8rem;
-}
-
-.days-temperature {
-  @include glass-morph();
-  display: flex;
-  justify-content: center;
-  gap: 0.8rem;
-}
-</style>
+<style lang="scss"></style>
