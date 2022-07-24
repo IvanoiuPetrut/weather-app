818e7e2aa3acf8efb58bb80a65cd95bdd9cd066f
Petrut <ivanoiu.petrut27@yahoo.com>
2022-07-24 22:45:31 +0300

    feat: start wroking on SearchBar style

--- src/components/SearchList.vue
+++ src/components/SearchList.vue
@@ -43,36 +43,15 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-* {
-  color: #fff;
-}
-
-@mixin glass-morph {
-  background: rgba(255, 255, 255, 0.15);
-  box-shadow: 0 8px 32px 0 rgba(34, 34, 35, 0.37);
-  backdrop-filter: blur(4px);
-  -webkit-backdrop-filter: blur(4px);
-  border-radius: 10px;
-  border: 1px solid rgba(255, 255, 255, 0.18);
-}
-
-@mixin flex-column {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
+@use "../assets/style/colors.scss";
 
 .list {
-  @include glass-morph();
-  @include flex-column();
-  align-items: flex-start;
-  gap: 0.2rem;
   padding: 0.6rem;
   &__element {
     padding: 0.2rem 0.4rem;
     cursor: pointer;
     &:hover {
-      color: #339af0;
+      color: colors.$accent-color;
     }
   }
 }
