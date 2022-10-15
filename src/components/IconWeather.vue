6e860185132cdd65fad9f5540eb4ee600394fb8e
Petrut <ivanoiu.petrut27@yahoo.com>
2022-10-15 19:46:31 +0300

    feat(style): add scss for the component

--- src/components/IconWeather.vue
+++ src/components/IconWeather.vue
@@ -508,6 +508,28 @@
   </svg>
 
   <svg
+    v-else-if="name === 'current-location'"
+    xmlns="http://www.w3.org/2000/svg"
+    class="icon icon-tabler icon-tabler-current-location"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    stroke-width="2"
+    stroke="currentColor"
+    fill="none"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  >
+    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+    <circle cx="12" cy="12" r="3"></circle>
+    <circle cx="12" cy="12" r="8"></circle>
+    <line x1="12" y1="2" x2="12" y2="4"></line>
+    <line x1="12" y1="20" x2="12" y2="22"></line>
+    <line x1="20" y1="12" x2="22" y2="12"></line>
+    <line x1="2" y1="12" x2="4" y2="12"></line>
+  </svg>
+
+  <svg
     v-else-if="name === 'arrow-left'"
     aria-hidden="true"
     focusable="false"
--- src/components/SelectLocation.vue
+++ src/components/SelectLocation.vue
@@ -1,30 +1,28 @@
 <template>
   <div class="select-location">
-    <div>
-      <h2>Weather</h2>
-      <IconWeather name="logo"></IconWeather>
+    <div class="header">
+      <h2 class="header__title">Get the weather</h2>
+      <IconWeather name="logo" class="header__logo"></IconWeather>
     </div>
-    <div class="form">
-      <label for="serach-ciy" class="form__label">Chose a city</label>
-      <div class="form__section">
-        <input type="text" id="serach-ciy" class="form__input" />
-        <button class="form__btn">Get Weather</button>
-      </div>
-    </div>
+    <SearchBar></SearchBar>
     <div class="get-your-location">
-      <h3 class="get-your-location__title">Let us get your location</h3>
-      <button class="get-your-location__btn">Location</button>
+      <button class="get-your-location__btn">
+        <IconWeather name="current-location" class="icon"></IconWeather>
+        Use my location
+      </button>
     </div>
   </div>
 </template>
 
 <script>
 import IconWeather from "./IconWeather.vue";
+import SearchBar from "./SearchBar.vue";
 
 export default {
   name: "SelectLocation",
   components: {
     IconWeather,
+    SearchBar,
   },
 };
 </script>
@@ -33,6 +31,54 @@ export default {
 @use "../assets/style/colors.scss";
 
 .select-location {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 2rem;
+  padding: 2rem 3.2rem;
+  max-width: 600px;
   background-color: colors.$primary-color;
+  border-radius: 13px;
+  border: 1px solid colors.$transparent-color-neutral;
+  .header {
+    display: flex;
+    align-items: center;
+    gap: 3.2rem;
+    margin-bottom: 2.4rem;
+
+    &__title {
+      font-size: 2rem;
+    }
+
+    &__logo {
+      width: 3.2rem;
+      height: 3.2rem;
+    }
+  }
+
+  .get-your-location__btn {
+    display: flex;
+    align-items: center;
+    gap: 0.8rem;
+    padding: 0.6rem 1rem;
+    font-size: 1rem;
+    font-weight: bold;
+    border-radius: 13px;
+    color: colors.$text-color;
+    background-color: colors.$secondary-color;
+    border: 1px solid colors.$transparent-color-neutral;
+    cursor: pointer;
+    transition: all 0.3s ease-in-out;
+
+    &:hover {
+      background-color: colors.$transparent-primary-color;
+      border-color: colors.$accent-color;
+    }
+
+    .icon {
+      width: 1.6rem;
+      height: 1.6rem;
+    }
+  }
 }
 </style>
